25731a4c1014f47693c93f8ad10909815d300bef
samarJ19 <samarjoshitech@gmail.com>
2024-09-11 19:22:05 +0530

    few changes in quizapicall

--- src/Components/Quiz/Quizapicall.jsx
+++ src/Components/Quiz/Quizapicall.jsx
@@ -1,32 +1,31 @@
 import { useEffect, useState } from 'react';
 import { CohereClient } from 'cohere-ai';
 const Quizapicall=({ subjects,differenetsub })=>{
-
+//How to format received content from api
 const cohere = new CohereClient({
   token: 'IA9vNMfHYZhBHoD3GJiqCz3BmeJWZzDgWT8AcuxR',
 });
 const [text,setText]=useState("")
 const [flag,setFlag]=useState(false);
 useEffect(()=>{
-    //How to send multiple questions !
-    //I am using the map function once i see the output then i can think about how can i do it!
+    
     (async () => {
         const questionsString = differenetsub.map((item) => item.queasked).join(', ');
         const answersString = differenetsub.map((item) => item.answer).join(', ');
-      const messageToSend = `I am giving you certain responses of a student to recommend him/her career options. The response includes marks on a certain test: Maths=${subjects.maths}, Arts=${subjects.arts}, Science=${subjects.science}. These are out of 20 in each subject. Additional questions which tell about the user are given first all the questions are given then all the answer are given respectively : questions: ${questionsString}, user's answers: ${answersString}`;
+      const messageToSend = `I am giving you certain responses about a student, recommend him/her career options. The responses includes marks on a certain test: Maths=${subjects.maths}, Arts=${subjects.arts}, Science=${subjects.science}. These are out of 20 in each subject. Additional questions which tell about the user are given first all the questions are given then all the answer are given respectively : questions: ${questionsString}, user's answers: ${answersString}, return the response in a manner so that it can be directly get rendered on the web-page`;
       console.log("The message to be sent: ", messageToSend);
         const response = await cohere.chat({
           message: messageToSend,
         });
         setText(response.text)
         setFlag(true)
-        console.log("The response is :",response)
+        console.log("The response is :",response.text)
       })
       ();
 },[])
     if(flag){
         return<div>
-            <p>{text}</p>
+            {text}
         </div>
     }
     return <div>
---
+++ src/Components/ResourcePortal/Resourcelandingpage.jsx
@@ -0,0 +1,14 @@
+
+const Resourcelandingpage=()=>{
+    return <div>
+        <div className="flex m-2">
+        <button>Career Guides</button>
+        <button>Educational Pathways</button>
+        <button>Study Materials</button>
+        <button>Skills and Certifications</button>
+        <button>Career Development Tools</button>
+        </div>
+    </div>
+}
+
+export default Resourcelandingpage;
